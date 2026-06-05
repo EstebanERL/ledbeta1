f053cdecb5d35a7c933e6579bf4f57ffb22f96d0
carlos sayago <carlojrsayagoberdugo@gmail.com>
2026-06-05 16:12:09 -0500

    fix vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,13 @@
 import 'dotenv/config';
 import { app } from './app.js';
 
+app.get("/", (req, res) => {
+  res.json({
+    status: "OK",
+    message: "TalentForge API running"
+  });
+});
+
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
